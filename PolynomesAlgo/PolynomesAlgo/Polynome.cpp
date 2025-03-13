b711588e5294f85300cf394650d3abdf5bb31701
Lume <l.niewolska@lahorde.school>
2025-03-13 12:13:01 +0100

    Sorted Add

--- PolynomesAlgo/PolynomesAlgo/Polynome.cpp
+++ PolynomesAlgo/PolynomesAlgo/Polynome.cpp
@@ -1,4 +1,4 @@
-#include "Polynome.h"
+#include "Polynome.h"#include "Polynome.h"
 #include <iostream>
 
 using namespace std;
@@ -8,57 +8,70 @@ Polynome::Polynome() {
 }
 
 void Polynome::Add(Monome* newMonome) {
-	if (head == nullptr) {
-		head = newMonome;
-		return;
-	}
+    if (head == nullptr) {
+        head = newMonome;
+        return;
+    }
 
-	if (head->exposant == newMonome->exposant) head->value += newMonome->value; //change later to factorise into Monome (?)
-	if (head->exposant > newMonome->exposant) {
-		newMonome->next = head;
-		head = newMonome;
-		return;
-	}
+    if (head->exposant == newMonome->exposant) {
+        head->value += newMonome->value; //change later to factorise into Monome (?)
+        return;
+    }
+    if (head->exposant > newMonome->exposant) {
+        newMonome->next = head;
+        head = newMonome;
+        return;
+    }
 
-	Monome* a = head;
-	while (a->next != nullptr && a->exposant < newMonome->exposant-1) {
-		a = a->next;
-	}
-	if (a->next != nullptr) { 
-		if (a->next->exposant == newMonome->exposant) {
-			a->next->value += newMonome->value; //change later to factorise into Monome
-			//cout << "a->next->exposant: " << a->next->exposant << " | newMonome->exposant: " << newMonome->exposant << " |||| ";
-		}
-		else {
-			AddAfterMonome(a, newMonome);
-			cout << "AddAfterMonome";
-		}
-	}
-	else {
-		AddToEndOfList(newMonome);
-	}
+    Monome* a = head;
+    Monome* prev = nullptr;
+    while (a->next != nullptr && a->exposant < newMonome->exposant - 1) {
+        prev = a;
+        a = a->next;
+    }
+
+    if (a->exposant == newMonome->exposant) {
+        a->value += newMonome->value;
+        return;
+    }
+
+    if (a->next != nullptr) {
+        if (a->next->exposant == newMonome->exposant) {
+            a->next->value += newMonome->value;
+        }
+        else {
+            AddAfterMonome(a, newMonome);
+        }
+    }
+    else {
+        if (a->exposant > newMonome->exposant) {
+            AddAfterMonome(prev, newMonome);
+            return;
+        }
+        AddToEndOfList(newMonome);
+    }
 }
 
 void Polynome::AddToEndOfList(Monome* newMonome) {
-	Monome* a = head;
+    Monome* a = head;
 
-	while (a->next != nullptr) { a = a->next; }
+    while (a->next != nullptr) { a = a->next; }
 
-	a->next = newMonome;
+    a->next = newMonome;
 }
 
 void Polynome::AddAfterMonome(Monome* monome, Monome* toAdd) {
-	toAdd->next  = monome->next;
-	monome->next = toAdd;
+    toAdd->next = monome->next;
+    monome->next = toAdd;
 }
 
 void Polynome::DisplayPolynome() {
-	Monome* a = head;
-	while (a != nullptr) {
-		if (a->next != nullptr) cout << a->value << "x^" << a->exposant << " + ";
-		else cout << a->value << "x^" << a->exposant;
-		a = a->next;
-	}
+    Monome* a = head;
+    while (a != nullptr) {
+        if (a->next != nullptr) cout << a->value << "x^" << a->exposant << " + ";
+        else cout << a->value << "x^" << a->exposant;
+        a = a->next;
+    }
 
-	cout << endl;
+    cout << endl;
 }
