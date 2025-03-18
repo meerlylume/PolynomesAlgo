12d163bc951f1f9174f2708c3a79ca2a36a2a223
LucyanaPanda <lucie.nguyenpham@gmail.com>
2025-03-18 16:01:52 +0100

    Adding Primitive

--- PolynomesAlgo/PolynomesAlgo/Monome.cpp
+++ PolynomesAlgo/PolynomesAlgo/Monome.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
-Monome::Monome(int value, int exposant) : value(value), exposant(exposant) {
+Monome::Monome(float value, float exposant) : value(value), exposant(exposant) {
 
+}
+
+Monome* Monome::PrimitiveMonome() {
+	float value = 0;
+	float exposant = 0;
+
+	exposant = this->exposant + 1;
+	value = this->value / exposant;
+
+	Monome* primitive = new Monome(value, exposant);
+	return primitive;
 }
